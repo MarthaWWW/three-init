6db087cce0c9a6786d8cf4df16bae90bd89a3b57
MarthaWWW <curtainofmartha@gmail.com>
2023-02-23 00:40:32 +0800

    安装elementPlus

--- three-init/src/views/HomeList.vue
+++ three-init/src/views/HomeList.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <h1>Say hello to three.js!</h1>
-    <ul class='list'>
-      <li><a>001-在场景中自定义光源绘制立方体</a></li>
-    </ul>
+  <el-container>
+  <el-header>Header</el-header>
+  <el-main>Main</el-main>
+  </el-container>
   </div>
 </template>
 <style lang="scss" scoped>
-.list{
-  float: left;
-}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
 </style>
